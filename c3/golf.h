68f3aa95fd94331dfe59a914d9a9f723b7183ce3
Phenomenon <1149650442@qq.com>
2019-03-10 20:46:02 +0800

    c3(ver 1.0)

---
+++ c3/golf.h
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+using std::string;
+class Golf
+{
+private:
+	struct golf
+	{
+		string fullname;
+		int handicap;
+	};
+public:
+	
+	//function displays contents of golf structure
+	void showgolf(const golf* gpt) {
+		using std::cout;
+		using std::endl;
+		cout << "Name: " << gpt->fullname << endl;
+		cout << "Handicap: " << gpt->handicap << endl;
+	}
+
+	Golf(string name, int hc) {
+		golf* gpt = new golf;
+		gpt->fullname = name;
+		gpt->handicap = hc;
+		showgolf(gpt);
+	}
+	//interactive version:
+	//function solicits name and handicap from user
+	//and sets the member of g to the values entered
+	Golf() {
+		using std::cin;
+		using std::cout;
+		using std::endl;
+		string name;
+		int hc;
+	M:	cout << "Enter your name: " << endl;
+		cin >> name;
+		cout << "Enter your handicap: " << endl;
+		cin >> hc;
+		if (name != "\0" && hc != '\0')
+			Golf(name, hc);
+		else
+		{
+			cout << "Please try again!";
+			goto M;
+		}
+	}
+
+	~Golf();
+};
+
+
+
+
+
+
+
+
+
+
+
